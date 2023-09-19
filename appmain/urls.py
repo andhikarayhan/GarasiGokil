7751fbeeb77f544ef3dad1abfda9b6f73a3bc5a8
andhika rayhan <emberem6@gmail.com>
2023-09-20 03:53:03 +0700

    menyelesaikan poin 2,3 , dan sebagian poin 4

--- appmain/urls.py
+++ appmain/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from appmain.views import create_item, show_main
+from appmain.views import (create_item, show_json, show_json_by_id, show_main,
+                           show_xml, show_xml_by_id)
 
 app_name = 'appmain'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-item', create_item, name='create_item'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
--- appmain/views.py
+++ appmain/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseRedirect
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -48,3 +49,19 @@ def create_item(request):
 
     context = {'form': form}
     return render(request, "create_item.html", context)
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
