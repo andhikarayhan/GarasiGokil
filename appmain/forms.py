fa98542828707f413cad334576976a1a26b56137
andhika rayhan <emberem6@gmail.com>
2023-09-20 03:02:55 +0700

    dekorasi form page

--- appmain/forms.py
+++ appmain/forms.py
@@ -1,9 +1,13 @@
+from django import forms
 from django.forms import ModelForm
 
 from appmain.models import Item
 
 
 class ItemForm(ModelForm):
+    AMOUNT_CHOICES = [(i, str(i)) for i in range(1, 11)]  # Assuming a range from 1 to 10
+
+    amount = forms.ChoiceField(choices=AMOUNT_CHOICES, label="Amount", initial=1, widget=forms.Select)
     class Meta:
         model = Item
         fields = ["brand", "model","amount", "engine_spec"]
